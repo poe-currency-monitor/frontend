7da0c2a40bfbc7bf7c50349a3004cda67b143264
Thomas Cazade <cazade.thomas@gmail.com>
2021-01-26 08:39:17 +0100

    Add babel-plugin-import to reduce antd bundle-size

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,18 @@
 /* eslint global-require: "off" */
 module.exports = {
+  babel: {
+    plugins: [
+      [
+        'import',
+        {
+          libraryName: 'antd',
+          // Use pre-built components CSS files.
+          style: 'css',
+        },
+      ],
+    ],
+  },
+
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
