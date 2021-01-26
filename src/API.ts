d65585975485c03095c727024cdb61a8b777a64c
Thomas Cazade <cazade.thomas@gmail.com>
2021-01-26 08:18:09 +0100

    Add API functions using fetch for each endpoint

---
+++ src/API.ts
@@ -0,0 +1,22 @@
+const ENDPOINT = 'http://localhost:4201/api';
+const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+};
+
+export type LoginResponse = {
+  token: string;
+  accountName: string;
+};
+
+export const postLogin = (poesessid: string): Promise<LoginResponse> =>
+  fetch(`${ENDPOINT}/auth/`, {
+    method: 'POST',
+    headers: { ...DEFAULT_HEADERS },
+    body: JSON.stringify({ poesessid }),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Unable to login: error ${response.status}`);
+    }
+
+    return response.json() as Promise<LoginResponse>;
+  });
