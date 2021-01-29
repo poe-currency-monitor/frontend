589f81644963d2139c4c0b624bc612fceedab28e
Thomas Cazade <cazade.thomas@gmail.com>
2021-01-29 08:59:00 +0100

    UseInterval to refetch stash-tabs items on Dashboard view

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 import React, { useContext, useEffect, useMemo } from 'react';
 import { useQuery } from 'react-query';
+import useInterval from '@use-it/interval';
 
 import { Item } from '../../interfaces/poe.interfaces';
 import { getStashTabsItems, StashTabsItemsResponse } from '../../API';
 import { UserContext } from '../../contexts/UserContext';
 import { useLogged } from '../../hooks/use-logged';
+import { useItemsDiff } from '../../hooks/use-items-diff';
 import { useCalculateWorth } from '../../hooks/use-calculate-worth';
 import { ItemsWorthTable } from '../../components/ItemsWorthTable';
+
+const FETCH_DELAY = 30000;
 
 /**
  * Format stash-tabs item API response into a usable object.
@@ -32,6 +36,7 @@ export const DashboardView: React.FC = () => {
 
   const league = useMemo(() => (selectedCharacter ? selectedCharacter.league : 'Standard'), [selectedCharacter]);
   const tabIndexes = useMemo(() => selectedStashTabs.map((tab) => tab.i).join(','), [selectedStashTabs]);
+  const stashTabsItemsMemo = useMemo(() => stashTabsItems || {}, [stashTabsItems]);
 
   const items = useQuery(['getStashTabsItems', poesessid, token, accountName, league, tabIndexes], () =>
     poesessid && token && accountName && league && tabIndexes
@@ -41,8 +46,8 @@ export const DashboardView: React.FC = () => {
 
   const itemsData = useMemo(() => formatItemsPayload(items.data), [items]);
 
+  const itemsDiff = useItemsDiff(itemsData, stashTabsItemsMemo);
   const { currencyItemsWorth } = useCalculateWorth(itemsData);
-
   const itemsWorth = useMemo(() => [...currencyItemsWorth], [currencyItemsWorth]);
 
   // Initial load of stash-tabs items.
@@ -51,7 +56,20 @@ export const DashboardView: React.FC = () => {
       const payload = formatItemsPayload(items.data);
       setStashTabsItems(payload);
     }
-  });
+  }, [items, stashTabsItems, setStashTabsItems]);
+
+  // Triggered each time stash-tabs items are fetched and when there are new
+  // items between the `UserContext.stashTabsItems` and `query.data.items`.
+  useEffect(() => {
+    if (!items.isLoading && items.data && stashTabsItems && itemsDiff) {
+      console.log('stash-tabs items refetched it contains new items', itemsDiff);
+    }
+  }, [items, stashTabsItems, itemsDiff]);
+
+  // Initialize interval to load items X seconds.
+  useInterval(() => {
+    items.refetch().catch(() => null);
+  }, FETCH_DELAY);
 
   return (
     <main className="flex flex-col justify-center max-w-5xl mx-auto">
