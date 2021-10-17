6854aaad678e20de2e70155a986e65fe478148ce
Thomas Cazade <cazade.thomas@gmail.com>
2021-10-17 19:42:13 +0200

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
 
   // Add this part if you are using the ESLint config on a TypeScript project
   parserOptions: {
-    tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
 };
