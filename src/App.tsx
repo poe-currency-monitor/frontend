8822332ba010362d4b9714abcc30f2a947b08b08
Thomas Cazade <cazade.thomas@gmail.com>
2021-10-17 22:16:03 +0200

    Add AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import { AppProvider } from './providers/AppProvider';
+import { AppRoutes } from './AppRoutes';
 
-const App: React.FC = () => <AppProvider />;
+const App: React.FC = () => (
+  <AppProvider>
+    <AppRoutes />
+  </AppProvider>
+);
 
 export default App;
---
+++ src/AppRoutes.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { RouteProps, BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import { UserContext } from './contexts/UserContext';
+
+export type ProtectedRouteProps = RouteProps & {
+  component: React.FC;
+};
+
+/**
+ * A protected `react-router-dom` `<Route />` component that need the user to
+ * have a `token` set in the `UserContext`.
+ *
+ * Automatically redirect to `/` if the condition is not satisfied.
+ */
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, ...props }) => {
+  const { token } = React.useContext(UserContext);
+
+  return (
+    <Route
+      {...props}
+      render={(renderProps) => {
+        if (token) {
+          return <Component />;
+        }
+
+        return <Redirect to={{ pathname: '/', state: { from: renderProps.location } }} />;
+      }}
+    />
+  );
+};
+
+/**
+ * Contains entire app routing logic with components to display for each route.
+ */
+export const AppRoutes: React.FC = () => (
+  <Router>
+    <Switch>{/* <Route path="/" exact component={Auth} /> */}</Switch>
+  </Router>
+);
