fa6f8789cbb6f10129c3b68a04b234129a1545f5
Thomas Cazade <cazade.thomas@gmail.com>
2021-10-19 14:24:49 +0200

    Add Profile creation modal

---
+++ src/features/profile/CreateProfileModal.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Modal from 'react-modal';
+import { XIcon } from '@heroicons/react/solid';
+
+export type CreateProfileModalTypes = Modal.Props & {
+  onClose: () => unknown;
+};
+
+export const CreateProfileModal: React.FC<CreateProfileModalTypes> = ({ isOpen, onClose, ...props }) => {
+  React.useEffect(() => {
+    Modal.setAppElement('#root');
+  }, []);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      className="relative max-w-3xl px-6 py-4 mx-auto rounded-md text-zinc-900 bg-slate-100"
+      {...props}
+    >
+      <button
+        type="button"
+        className="absolute top-0 right-0 h-auto w-auto mt-4 mr-4 cursor-pointer focus:outline-none"
+      >
+        <XIcon className="h-6 w-auto" onClick={onClose} onKeyPress={onClose} />
+      </button>
+
+      <h3 className="mb-8 font-medium text-2xl text-center">Create a new profile</h3>
+
+      <form className="max-w-sm mx-auto">
+        <label className="flex flex-col mb-4" htmlFor="profile-name">
+          <span className="mb-1 font-medium">Enter your profile name</span>
+
+          <input
+            className="transition px-4 py-2 rounded-md text-lg text-zinc-900 font-medium ring-blue-500 focus:outline-none focus:ring placeholder-zinc-400"
+            id="profile-name"
+            type="text"
+            placeholder="Profile name"
+          />
+        </label>
+      </form>
+    </Modal>
+  );
+};
--- src/features/profile/ProfileList.tsx
+++ src/features/profile/ProfileList.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
-
 import { PlusCircleIcon } from '@heroicons/react/solid';
 
-export const ProfileList: React.FC = () => (
-  <ul className="grid grid-cols-3 gap-4 max-w-3xl min-h-[192px] mx-auto">
-    <li className="transition flex flex-col items-center justify-center h-full px-2 py-4 border-2 border-dashed border-zinc-500 rounded-md cursor-pointer hover:border-zinc-400">
-      <PlusCircleIcon className="block h-10 w-auto mb-3" />
+import { CreateProfileModal } from './CreateProfileModal';
 
-      <p className="text-base font-medium">Create new profile</p>
-    </li>
-  </ul>
-);
+export const ProfileList: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  return (
+    <>
+      <ul className="grid grid-cols-3 gap-4 max-w-3xl min-h-[192px] mx-auto">
+        <li className="h-full w-full">
+          <button
+            type="button"
+            className="transition flex flex-col items-center justify-center h-full w-full px-2 py-4 border-2 border-dashed border-zinc-500 rounded-md cursor-pointer hover:border-zinc-400 focus:outline-none"
+            onClick={() => setIsModalOpen(true)}
+            onKeyPress={(event) => (event.key === 'Enter' ? setIsModalOpen(true) : null)}
+          >
+            <PlusCircleIcon className="block h-10 w-auto mb-3" />
+
+            <p className="text-base font-medium">Create new profile</p>
+          </button>
+        </li>
+      </ul>
+
+      <CreateProfileModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
+  );
+};
