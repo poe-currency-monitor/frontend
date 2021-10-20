002f3746ec31d62c123262157a726ffd57eba7e0
Thomas Cazade <cazade.thomas@gmail.com>
2021-10-20 19:28:48 +0200

    Load stash-tabs, items and rates after selecting a profile

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,14 +1,93 @@
 import * as React from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { StashTabsItems } from '../../interfaces/poe.interfaces';
 import { Profile as ProfileType } from '../../interfaces/profile.interfaces';
 import { UserContext } from '../../contexts/UserContext';
+import { useLoadStashTabs } from '../../hooks/useLoadStashTabs';
+import { useLoadRates } from '../../hooks/useLoadRates';
+import { useLoadStashItems } from '../../hooks/useLoadStashItems';
 import { ProfileList } from './ProfileList';
 
 export const Profile: React.FC = () => {
   const history = useHistory();
 
   const user = React.useContext(UserContext);
+
+  const tabsIndexes = React.useMemo<string>(() => {
+    const filteredFullStashTabs = user.stashTabs.filter((stashTab) =>
+      user.currentProfile?.tabs.find((profileTab) => profileTab.id === stashTab.id),
+    );
+
+    return filteredFullStashTabs.map((stashTab) => stashTab.i).join('');
+  }, [user.stashTabs, user.currentProfile]);
+
+  const { fetchCurrenciesRates, fetchItemsRates } = useLoadRates(user.token, user.currentProfile?.league);
+
+  const { fetchStashTabs } = useLoadStashTabs(
+    user.token,
+    user.poesessid,
+    user.currentProfile?.league,
+    user.accountName,
+  );
+
+  const { fetchStashItems } = useLoadStashItems(
+    user.token,
+    user.poesessid,
+    user.currentProfile?.league,
+    user.accountName,
+    tabsIndexes,
+  );
+
+  // Load currencies and items rates when a profile has been selected.
+  React.useEffect(() => {
+    if (user.currentProfile && !fetchCurrenciesRates.data && !fetchItemsRates.data) {
+      // Rates are automatically added to RatesContext inside the query definition.
+      fetchCurrenciesRates.refetch().catch(() => null);
+      fetchItemsRates.refetch().catch(() => null);
+    }
+  }, [user, fetchCurrenciesRates, fetchItemsRates]);
+
+  // When profile is selected, this effect is called, so we can load stash-tabs,
+  // items-rates and stash-tabs items.
+  React.useEffect(() => {
+    if (user.currentProfile && !fetchStashTabs.data) {
+      fetchStashTabs
+        .refetch()
+        .then((response) => (response.data ? user.setStashTabs(response.data.tabs.tabs) : null))
+        .catch(() => null);
+    }
+  }, [user, fetchStashTabs]);
+
+  // Once stash-tabs have been loaded, `tabsIndexes` should have been updated
+  // so we can fetch stash-items.
+  React.useEffect(() => {
+    if (user.currentProfile && tabsIndexes && !fetchStashItems.data) {
+      fetchStashItems
+        .refetch()
+        .then((response) => {
+          if (response.data) {
+            const stashTabsItems: StashTabsItems = {};
+
+            Object.entries(response.data.items).forEach(([key, value]) => {
+              stashTabsItems[key] = value.items;
+            });
+
+            user.setStashTabsItems(stashTabsItems);
+          }
+
+          return response;
+        })
+        .catch(() => null);
+    }
+  }, [user, tabsIndexes, fetchStashItems]);
+
+  // Once everything has been loaded, we can redirect to the dashboard view.
+  React.useEffect(() => {
+    if (fetchCurrenciesRates.data && fetchItemsRates.data && fetchStashTabs.data && fetchStashItems.data) {
+      history.push('/dashboard');
+    }
+  }, [history, fetchCurrenciesRates, fetchItemsRates, fetchStashTabs, fetchStashItems]);
 
   const handleDeleteProfile = (profile: ProfileType) => {
     const newProfiles = user.profiles.filter((p) => p.name !== profile.name);
@@ -18,7 +97,6 @@ export const Profile: React.FC = () => {
 
   const handleSelectProfile = (profile: ProfileType) => {
     user.setCurrentProfile(profile);
-    history.push('/dashboard');
   };
 
   return (
