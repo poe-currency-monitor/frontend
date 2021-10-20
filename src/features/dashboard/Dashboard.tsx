efe7c0a1d5c315bcb63fd26bc5f2ce3cf5b152e4
Thomas Cazade <cazade.thomas@gmail.com>
2021-10-20 11:19:33 +0200

    Load rates on Dashboard view

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react';
 
+import { UserContext } from '../../contexts/UserContext';
+import { useLoadRates } from '../../hooks/useLoadRates';
+import { useOnMount } from '../../hooks/useOnMount';
+
 export const Dashboard: React.FC = () => {
+  const user = React.useContext(UserContext);
+
+  const { triggerRatesRequests } = useLoadRates(user.token, user.currentProfile?.league);
+
+  useOnMount(() => {
+    triggerRatesRequests().catch(() => null);
+  });
+
   return null;
 };
