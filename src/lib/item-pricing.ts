8881e6c36ec39f5e890c11ca3112bc2cec561923
Thomas Cazade <cazade.thomas@gmail.com>
2021-11-02 17:08:50 +0100

    Price essence items

--- src/lib/item-pricing.ts
+++ src/lib/item-pricing.ts
@@ -19,12 +19,23 @@ export function priceItem(item: Item, rates: RatesContextType): PricedItem {
     total: 0,
   };
 
+  // Price a common currency item.
   if (rates.currencyRates && item.frameType === 5) {
     const currencyRate = rates.currencyRates.lines.find((line) => line.currencyTypeName === item.typeLine);
 
     if (currencyRate) {
       itemValue.unit = currencyRate.chaosEquivalent;
       itemValue.total = currencyRate.chaosEquivalent * (item.stackSize || 1);
+    }
+  }
+
+  // Price an essence currency item.
+  if (rates.essenceRates && item.frameType === 5) {
+    const essenceRate = rates.essenceRates.lines.find((line) => line.name === item.typeLine);
+
+    if (essenceRate) {
+      itemValue.unit = essenceRate.chaosValue;
+      itemValue.total = essenceRate.chaosValue * (item.stackSize || 1);
     }
   }
 
