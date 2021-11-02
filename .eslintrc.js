7eac578dbf39cef3df89d26116b07ba4caa0f3a9
Thomas Cazade <cazade.thomas@gmail.com>
2021-11-02 11:32:24 +0100

    Upgrade to latest version of ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
   extends: ['@totominc/react'],
 
-  // Add this part if you are using the ESLint config on a TypeScript project
   parserOptions: {
-    project: ['./tsconfig.json'],
-  },
-
-  rules: {
-    'react/jsx-props-no-spreading': 'off',
+    project: './tsconfig.json',
   },
 };
