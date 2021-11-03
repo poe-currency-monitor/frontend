0cc9c53a5907b54a79922f71468b06bf7b3c4c2b
Thomas Cazade <cazade.thomas@gmail.com>
2021-11-03 13:39:12 +0100

    Update order of leagues

--- src/data/leagues.ts
+++ src/data/leagues.ts
@@ -5,34 +5,6 @@ import { League } from '../interfaces/poe.interfaces';
  */
 export const leagues: League[] = [
   {
-    id: 'Standard',
-    realm: 'pc',
-    url: 'https://www.pathofexile.com/forum/view-thread/71278',
-    startAt: '2013-01-23T21:00:00Z',
-    endAt: null,
-    description: 'The default game mode.',
-    registerAt: '2019-09-06T19:00:00Z',
-    delveEvent: true,
-    rules: [],
-  },
-  {
-    id: 'Hardcore',
-    realm: 'pc',
-    url: 'https://www.pathofexile.com/forum/view-thread/71276',
-    startAt: '2013-01-23T21:00:00Z',
-    endAt: null,
-    description: 'A character killed in the Hardcore league is moved to the Standard league.',
-    registerAt: '2019-09-06T19:00:00Z',
-    delveEvent: true,
-    rules: [
-      {
-        id: 'Hardcore',
-        name: 'Hardcore',
-        description: 'A character killed in Hardcore is moved to its parent league.',
-      },
-    ],
-  },
-  {
     id: 'Scourge',
     realm: 'pc',
     url: 'https://www.pathofexile.com/forum/view-thread/3188271',
@@ -53,6 +25,34 @@ export const leagues: League[] = [
     description:
       'Defeat The Scourge in an alternate Wraeclast to create powerful corrupted items.\n\nA character killed in Hardcore Scourge becomes a Standard character.',
     registerAt: '2021-10-22T16:30:00Z',
+    delveEvent: true,
+    rules: [
+      {
+        id: 'Hardcore',
+        name: 'Hardcore',
+        description: 'A character killed in Hardcore is moved to its parent league.',
+      },
+    ],
+  },
+  {
+    id: 'Standard',
+    realm: 'pc',
+    url: 'https://www.pathofexile.com/forum/view-thread/71278',
+    startAt: '2013-01-23T21:00:00Z',
+    endAt: null,
+    description: 'The default game mode.',
+    registerAt: '2019-09-06T19:00:00Z',
+    delveEvent: true,
+    rules: [],
+  },
+  {
+    id: 'Hardcore',
+    realm: 'pc',
+    url: 'https://www.pathofexile.com/forum/view-thread/71276',
+    startAt: '2013-01-23T21:00:00Z',
+    endAt: null,
+    description: 'A character killed in the Hardcore league is moved to the Standard league.',
+    registerAt: '2019-09-06T19:00:00Z',
     delveEvent: true,
     rules: [
       {
