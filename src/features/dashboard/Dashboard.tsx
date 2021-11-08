7f902289412c6da2286089ddef6f438c3eed4a41
Thomas Cazade <cazade.thomas@gmail.com>
2021-11-08 15:42:45 +0100

    Create a snapshot on Dashboard component mounted

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+import { priceStashTabsItems } from '../../lib/item-pricing';
+import { UserContext } from '../../contexts/UserContext';
+import { RatesContext } from '../../contexts/RatesContext';
 import { Layout } from '../../components/Layout';
 import { DashboardItemsTable } from './DashboardItemsTable';
 
@@ -7,6 +10,26 @@ import { DashboardItemsTable } from './DashboardItemsTable';
  * View component for the `/dashboard` route.
  */
 export const Dashboard: React.FC = () => {
+  const user = React.useContext(UserContext);
+  const rates = React.useContext(RatesContext);
+
+  // On component mounted, create a snapshot of the user's stash.
+  React.useEffect(() => {
+    if (user.currentProfile) {
+      const snapshots = user.currentProfile.snapshots;
+      const date = new Date().toISOString();
+
+      user.currentProfile.snapshots = [
+        ...snapshots,
+        {
+          date,
+          stashTabs: user.stashTabs,
+          items: priceStashTabsItems(user.stashTabsItems, rates),
+        },
+      ];
+    }
+  }, []);
+
   return (
     <Layout>
       <h1 className="mb-6 leading-tight text-3xl font-bold">Dashboard</h1>
