8e530bcb4ad4fa7c791f0abad2d49878295d74a5
Thomas Cazade <cazade.thomas@gmail.com>
2021-11-08 17:29:58 +0100

    Add snapshot button on Dashboard

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { ClockIcon } from '@heroicons/react/solid';
 
 import { priceStashTabsItems } from '../../lib/item-pricing';
 import { UserContext } from '../../contexts/UserContext';
 import { RatesContext } from '../../contexts/RatesContext';
 import { Layout } from '../../components/Layout';
+import { Button } from '../../components/ui/Button';
 import { DashboardItemsTable } from './DashboardItemsTable';
 
 /**
@@ -33,6 +35,13 @@ export const Dashboard: React.FC = () => {
   return (
     <Layout>
       <h1 className="mb-6 leading-tight text-3xl font-bold">Dashboard</h1>
+
+      <div className="flex flex-row-reverse mb-4">
+        <Button variant="secondary" className="flex flex-row" type="button">
+          <ClockIcon className="h-4 w-auto mr-2" />
+          {user.currentProfile?.snapshots.length || 0} snapshots
+        </Button>
+      </div>
 
       <DashboardItemsTable />
     </Layout>
