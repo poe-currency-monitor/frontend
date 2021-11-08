e3cc5854aea4e1056374cf2431ea878fc95e6ee4
Thomas Cazade <cazade.thomas@gmail.com>
2021-11-08 15:41:13 +0100

    Update snapshot type of profile

--- src/interfaces/profile.interfaces.ts
+++ src/interfaces/profile.interfaces.ts
@@ -18,6 +18,7 @@ export type Snapshot = {
    * Date string to ISO format.
    */
   date: string;
-  stashTabs: StashTab;
-  items: { [key: string]: PricedItem[] };
+  stashTabs: StashTab[];
+  // TODO: keep track of what stash-tab does the item comes from.
+  items: PricedItem[];
 };
